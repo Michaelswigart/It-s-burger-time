4aab53cd8cf4f4f11178b6cf4c9e8c720d2df4a0
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-03-16 23:27:08 -0500

    update.2.0

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,45 +1,37 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 var router = express.Router();
-// burger.js no code
+
 var burger = require(path.resolve(__dirname, "../models/burger.js"));
 
 router.get("/", (req, res) => {
-  burger.all(data => {
-      var handleBarsObj = {
-          burgers: data 
-      }
-      res.render("index", handleBarsObj)
+  burger.all((data) => {
+    var handleBarsObj = {
+      burgers: data,
+    };
+    res.render("index", handleBarsObj);
   });
 });
 
 router.post("/burgers", (req, res) => {
-  console.log(">",req.body.burgers)
- burger.create(['burgers'], [req.body.burgers], data => {
-  res.redirect("/");
-});
+  console.log(">", req.body.burger_name);
+  burger.create(["burger_name"], [req.body.burger_name], (data) => {
+    res.redirect("/");
+  });
 });
 
 router.put("/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
-  burger.update({
-      devoured: true
-  }, condition, function (data) {
+  console.log("condition: ", condition);
+  burger.update(
+    {
+      devoured: true,
+    },
+    condition,
+    function (data) {
       res.redirect("/");
-
-  });
+    }
+  );
 });
 
-// router.put("burgers/:id", (req, res) => {
-//  burger.update({devoured: (req.body.devoured) ? true:false},
-//  {
-//      id: req.params.id
-//  }, data => {
-//   if (data.changedRows === 0) {
-//       return res.status(404).end()
-//   }
-//   res.status(200).end()
-//  });
-// });
 module.exports = router;
